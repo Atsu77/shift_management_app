c4fd3cd43211989c57f09e04d5fd93cea5c6042c
Atsu77 <kamnknon@gmail.com>
2021-09-29 12:13:36 +0900

    講師用の新規登録のテストコードを作成

--- api/spec/requests/api/v1/teachers_spec.rb
+++ api/spec/requests/api/v1/teachers_spec.rb
@@ -4,12 +4,13 @@ describe 'TeacherApi' do
   it 'ユーザーを登録する' do
     teacher_attributes = FactoryBot.attributes_for(:teacher)
 
-    post path, params: {
-      name: teacher_attributes.name,
-      email: teacher_attributes.email,
+    post api_v1_teacher_registration_path, params: {
+      name: teacher_attributes[:name],
+      email: teacher_attributes[:email],
       password: 'password',
-      password_confirmation: 'password'
+      password_confirmation: 'password',
     }
     expect(response).to have_http_status(:success)
   end
+
 end
