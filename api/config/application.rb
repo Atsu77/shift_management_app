a2781fce5c1b65f3d32305077f885178cdaf04c6
Atsu77 <kamnknon@gmail.com>
2021-09-29 17:53:21 +0900

    application.rbから不要なコードを削除

--- api/config/application.rb
+++ api/config/application.rb
@@ -58,7 +58,7 @@ module Myapp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    config.i18n.default_locale = :ja
+    #config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
