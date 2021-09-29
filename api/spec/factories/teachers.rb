fd414cf3e06fb09066adc37b9e5453896c70ae3d
Atsu77 <kamnknon@gmail.com>
2021-09-29 11:26:06 +0900

    teachersのfacotrybotファイルを作成

--- api/spec/factories/teachers.rb
+++ api/spec/factories/teachers.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :teacher do
-    name 'Quill'
+    name { 'Quill' }
     sequence(:email) { |n| "sample#{n}@example.com" }
-    password_digest "password"
-    gender 'male'
-    address 'Tokyo'
-    number '090-9999-9999'
-    profile 'aaaaaaaaaaaaa'
+    password_digest { 'password' }
+    gender { 'male' }
+    address { 'Tokyo' }
+    number { '090-9999-9999' }
+    profile { 'aaaaaaaaaaaaa' }
   end
 end
