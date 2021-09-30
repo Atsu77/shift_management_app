825bb6c90de61dceef377117bd7da0ad0732a577
Atsu77 <kamnknon@gmail.com>
2021-09-30 12:16:02 +0900

    studentのmigrationファイルを作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'Teacher', at: 'auth', controller: {
         registrations: 'api/v1/auth/registrations'
       }
+      mount_devise_token_auth_for 'Student', at: 'auth', controller: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end
