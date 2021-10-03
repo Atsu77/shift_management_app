1067fd11788064124082c9af0ddfe0a3d4cc5701
Atsu77 <kamnknon@gmail.com>
2021-10-03 17:58:53 +0900

    specialtyのスペルが間違っていたので訂正

--- api/db/migrate/20211002045508_create_specialty.rb
+++ api/db/migrate/20211002045508_create_specialty.rb
@@ -1,6 +1,6 @@
-class CreateSpeciality < ActiveRecord::Migration[6.1]
+class CreateSpecialty < ActiveRecord::Migration[6.1]
   def change
-    create_table :specialities do |t|
+    create_table :specialties do |t|
       t.references :teacher, foreign_key: true
       t.references :subject, foreign_key: true
 
